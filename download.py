28a1c41f072a7d05133c1d64056a314d4a5d4877
Javier Sanchez <javiersanp@gmail.com>
2017-03-28 13:22:05 +0100

    Adds download module

---
+++ download.py
@@ -0,0 +1,50 @@
+import sys
+import requests
+import re
+
+default_bar_len = 60
+number_of_retries = 3
+default_timeout = 10
+chunk_size = 1024
+
+
+class ProgressBar():
+    """Basic text progress bar"""
+
+    def __init__(self, total):
+        """Initializes with total"""
+        self.total = total
+        self.bar_len = default_bar_len
+        self.progress = 0
+        
+    def update(self, step = 1):
+        """Increments progress by step and displays the percent as a bar"""
+        self.progress += step
+        if self.progress > self.total: self.progress = self.total
+        self.percent = round(100.0 * self.progress / float(self.total), 1)
+        filled_len = int(round(self.percent) * self.bar_len / 100.0)
+        bar = '#' * filled_len + '-' * (self.bar_len - filled_len)
+        sys.stdout.write('[%s] %s%%\r' % (bar, self.percent))
+        if self.progress == self.total:
+            sys.stdout.write('\n')
+        sys.stdout.flush()
+        
+
+def get_response(url, stream=False):
+    """Try many times to get a http response or raise exception"""
+    for i in range(number_of_retries):
+        response = requests.get(url, stream=stream, timeout=default_timeout)
+        if response.status_code == requests.codes.ok:
+            return response
+    response.raise_for_status()
+
+def wget(url, filename):
+    """Download url to filename"""
+    response = get_response(url, stream=True)
+    total = int(response.headers['Content-Length'])
+    progress = ProgressBar(total)
+    with open(filename, "wb") as f:
+        for chunk in response.iter_content(chunk_size):
+            progress.update(chunk_size)
+            f.write(chunk)
+
---
+++ test/test_download.py
@@ -0,0 +1,73 @@
+import unittest
+import mock
+import random
+
+from download import ProgressBar, get_response, wget, chunk_size
+
+
+class TestProgressBar(unittest.TestCase):
+
+    def test_init(self):
+        p = ProgressBar(1000)
+        self.assertEquals(p.total, 1000)
+        self.assertEquals(p.progress, 0)
+
+    @mock.patch('download.sys')
+    def test_update(self, mock_sys):
+        progress = random.randint(0,10)
+        pb = ProgressBar(1000)
+        pb.update(100 * progress)
+        self.assertEquals(pb.progress, 100 * progress)
+        self.assertEquals(mock_sys.stdout.write.call_count, 1)
+        output = mock_sys.stdout.write.call_args_list[0][0][0]
+        self.assertEquals(output.count('#'), int(pb.bar_len * progress / 10.0))
+        self.assertEquals(output.count('-'), int(pb.bar_len * (10-progress) / 10.0))
+
+    @mock.patch('download.sys')
+    def test_update100(self, mock_sys):
+        pb = ProgressBar(1000)
+        pb.update(1100)
+        self.assertEquals(pb.progress, 1000)
+        self.assertEquals(mock_sys.stdout.write.call_count, 2)
+        output = mock_sys.stdout.write.call_args_list[0][0][0]
+        self.assertEquals(output.count('#'), pb.bar_len)
+        self.assertEquals(output.count('-'), 0)
+        self.assertEquals(mock_sys.stdout.write.call_args_list[1][0][0], '\n')
+
+
+class TestGetResponse(unittest.TestCase):
+    
+    @mock.patch('download.requests')
+    def test_get_response_ok(self, mock_requests):
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 200
+        mock_requests.codes.ok = 200
+        mock_requests.get.return_value = mock_response
+        r = get_response('foo', 'bar')
+        self.assertEquals(r, mock_response)
+        mock_requests.get.assert_called_once_with('foo', stream='bar', timeout=10)
+
+    @mock.patch('download.requests')
+    def test_get_response_bad(self, mock_requests):
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 404
+        mock_requests.codes.ok = 200
+        mock_requests.get.return_value = mock_response
+        get_response('foo', 'bar')
+        self.assertEquals(mock_requests.get.call_count, 3)
+        mock_response.raise_for_status.assert_called_once_with()
+
+
+class TestWget(unittest.TestCase):
+
+    @mock.patch('download.get_response')
+    @mock.patch('download.ProgressBar')
+    @mock.patch('download.open')
+    def test_wget(self, mock_open, mock_pb, mock_gr):
+        mock_gr.return_value = mock.MagicMock()
+        mock_gr.return_value.iter_content = range
+        file_mock = mock.MagicMock()
+        mock_open.return_value = mock.MagicMock()
+        mock_open.return_value.__enter__.return_value = file_mock
+        wget('foo', 'bar')
+        self.assertEquals(file_mock.write.call_count, chunk_size)
