8210ee6946ad497d6129b3b00bcca2345c5f060d
Javier Sánchez <javiersanp@gmail.com>
2017-06-29 16:27:19 +0100

    Fix encoding problem

--- catatom2osm.py
+++ catatom2osm.py
@@ -146,7 +146,7 @@ class CatAtom2Osm:
                 building.append(other_gml)
                 del other_gml
             else:
-                log.info(_("The layer '%s' is empty"), u'otherconstruction')
+                log.info(_("The layer '%s' is empty"), 'otherconstruction')
             building.remove_outside_parts()
             building.explode_multi_parts()
             building.remove_parts_below_ground()
