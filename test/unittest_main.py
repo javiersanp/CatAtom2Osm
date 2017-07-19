969973305974fc2eee7a49cf088708b41ac6deb1
Javier Sánchez <javiersanp@gmail.com>
2017-07-19 07:43:21 +0100

    Adds to last commit

--- test/unittest_main.py
+++ test/unittest_main.py
@@ -2,21 +2,6 @@
 
 # copied directly from 2.7's unittest/__main__.py b/c coverage can't do -m
 
-from qgis.core import QgsApplication
-import gdal
-
-
-class QgsSingleton(QgsApplication):
-    _qgs = None
-    
-    def __new__(cls):
-        if QgsSingleton._qgs is None:
-            QgsSingleton._qgs = QgsApplication([], False)
-            QgsSingleton._qgs.initQgis()
-            gdal.SetConfigOption('GML_ATTRIBUTES_TO_OGR_FIELDS', 'YES')
-            gdal.SetConfigOption('GML_SKIP_RESOLVE_ELEMS', 'ALL')
-        return QgsSingleton._qgs
-    
 
 if __name__ == "__main__":
     import sys
