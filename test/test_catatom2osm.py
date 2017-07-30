d30850cb06d2fe5aaa64f6dd1f035bc5cde81edf
Javier Sánchez <javiersanp@gmail.com>
2017-07-30 23:55:37 +0100

    Tests main and catatom2osm

--- test/test_catatom2osm.py
+++ test/test_catatom2osm.py
@@ -1,23 +1,79 @@
 # -*- coding: utf-8 -*-
-import os
 import mock
-from qgis.core import *
-
-import main
 import unittest
-import setup
+from cStringIO import StringIO
+import codecs
+from contextlib import contextmanager
+import os, sys
+
+os.environ['LANGUAGE'] = 'C'
+import main
 import hgwnames
-from catatom2osm import *
+import setup
+import osm
+import layer
+from osmxml import etree
+import catatom2osm as cat
+
+@contextmanager
+def capture(command, *args, **kwargs):
+    out = sys.stdout
+    sys.stdout = StringIO()#codecs.getwriter('utf-8')(StringIO())
+    try:
+        command(*args, **kwargs)
+        sys.stdout.seek(0)
+        yield sys.stdout.read()
+    finally:
+        sys.stdout = out
+
+prov_atom = """
+<feed xmlns="http://www.w3.org/2005/Atom" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:georss="http://www.georss.org/georss"  xmlns:inspire_dls = "http://inspire.ec.europa.eu/schemas/inspire_dls/1.0" xml:lang="en"> 
+<title>Download Office foobar</title>
+<entry>
+<title> 09001-FOO buildings</title>
+</entry>
+<entry>
+<title> 09002-BAR buildings</title>
+</entry>
+<entry>
+<title> 09999-TAZ buildings</title>
+<georss:polygon>42.0997821981015 -3.79048777556759 42.0997821981015 -3.73420761211555 42.1181603073135 -3.73420761211555 42.1181603073135 -3.79048777556759 42.0997821981015 -3.79048777556759</georss:polygon>
+</entry>
+</feed>
+"""
 
 
 class TestCatAtom2Osm(unittest.TestCase):
 
     def setUp(self):
-        self.options = dict(tasks=False, zoning=False, building=False, 
-            address=False, parcel=False, log_level="DEBUG")
+        self.options = {'building': False, 'all': False, 'tasks': True, 
+            'log_level': 'INFO', 'parcel': False, 'list': False, 'zoning': True, 
+            'version': False, 'address': False}
+
+    def tearDown(self):
+        if os.path.exists('09999'):
+            os.rmdir('09999')
+
+    def test_write_task(self):
+        app = cat.CatAtom2Osm('09999', self.options)
+        app.urban_zoning = 0
+        app.utaskn = 100
+        app.rtaskn = 1
+        app.osm_from_layer = mock.MagicMock(return_value = 'foo')
+        app.merge_address = mock.MagicMock(return_value = 'bar')
+        app.write_osm = mock.MagicMock()
+        app.write_task(0, None)
+        app.write_osm.assert_called_once_with('foo', 'tasks/u00100.osm')
+        app.write_task(1, None, '')
+        app.write_osm.assert_called_with('foo', 'tasks/r001.osm')
+        app.merge_address.called_once_with('foo', 'bar')
+        app.write_task(0, None)
+        app.write_osm.assert_called_with('foo', 'tasks/u00101.osm')
+        app.write_task(1, None)
+        app.write_osm.assert_called_with('foo', 'tasks/r002.osm')
 
     def test_get_translations(self):
-        app = CatAtom2Osm('09999', self.options)
+        app = cat.CatAtom2Osm('09999', self.options)
         address = mock.MagicMock()
         address.get_highway_names = mock.MagicMock(return_value = {})
         highway_types_path = os.path.join(setup.app_path, 'highway_types.csv')
@@ -34,5 +90,52 @@ class TestCatAtom2Osm(unittest.TestCase):
         self.assertFalse(is_new)
         os.remove(highway_types_path)
         os.remove(highway_names_path)
-        os.rmdir('09999')
+
+    def test_get_current_ad_osm(self):
+        d = osm.Osm()
+        d.Node(0,0, {'addr:housenumber': '12', 'addr:street': 'foobar'})
+        d.Node(1,1, {'addr:housenumber': '14', 'addr:street': 'foobar'})
+        d.Node(2,2, {'addr:housenumber': '10', 'addr:place': 'bartaz'})
+        d.Node(3,3)
+        app = cat.CatAtom2Osm('09999', self.options)
+        app.read_osm = mock.MagicMock(return_value = d)
+        address = app.get_current_ad_osm()
+        self.assertEquals(address, set(['foobar14', 'foobar12', 'bartaz10']))
+
+    @mock.patch('catatom2osm.log.warning')
+    @mock.patch('catatom2osm.download')
+    def test_get_boundary(self, m_download, m_log):
+        bbox09999 = "41.9997821981,-3.83420761212,42.1997821981,-3.63420761212"
+        mun_json = '{"elements": [{"id": 1, "tags": {"name": "Barcelona"}}, ' \
+            '{"id": 2, "tags": {"name": "Tazmania"}}, {"id": 3, "tags": {"name": "Foo"}}]}'
+        m1 = mock.MagicMock()
+        m1.content = prov_atom
+        m2 = mock.MagicMock()
+        m2.text = mun_json
+        m_download.get_response.side_effect = [m1, m2]
+        app = cat.CatAtom2Osm('09999', self.options)
+        app.get_boundary()
+        url1 = setup.prov_url['BU'] % ('09', '09')
+        url2 = setup.boundary_query % bbox09999
+        m_download.get_response.assert_has_calls([mock.call(url1), 
+            mock.call(url2)], any_order=False)
+        self.assertEquals(app.boundary_id, 2)
+        self.assertEquals(app.boundary_name, 'Tazmania')
+        self.assertEquals(app.boundary_bbox, bbox09999)
+        m2.text = '{"elements": []}'
+        m_download.get_response.side_effect = [m1, m2]
+        app = cat.CatAtom2Osm('09999', self.options)
+        app.get_boundary()
+        output = m_log.call_args_list[0][0][0]
+        self.assertIn("Failed to find", output)
+
+    @mock.patch('catatom2osm.download')
+    def test_list_municipalities(self, m_download):
+        url = setup.prov_url['BU'] % ('99', '99')
+        m_download.get_response.return_value.content = prov_atom
+        with capture(cat.list_municipalities, '99') as output:
+            m_download.get_response.assert_called_once_with(url)
+            self.assertIn('foobar', output)
+            self.assertIn('FOO', output)
+            self.assertIn('BAR', output)
 
---
+++ test/test_main.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+import mock
+import unittest
+import sys, os
+import logging
+from cStringIO import StringIO
+import codecs
+from contextlib import contextmanager
+import setup
+os.environ['LANGUAGE'] = 'C'
+import main
+
+@contextmanager
+def capture(command, *args, **kwargs):
+    out = sys.stdout
+    sys.stdout = codecs.getwriter('utf-8')(StringIO())
+    try:
+        command(*args, **kwargs)
+        sys.stdout.seek(0)
+        yield sys.stdout.read()
+    finally:
+        sys.stdout = out
+
+def raiseIOError(*args, **kwargs):
+    raise IOError('bartaz')
+
+def raiseImportError(*args, **kwargs):
+    raise ImportError('qgis')
+
+
+class TestMain(unittest.TestCase):
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py'])
+    def test_no_args(self):
+        with capture(main.run) as output:
+            self.assertIn("Usage: catatom2osm.py", output)
+
+    @mock.patch('setup.platform', 'winx')
+    @mock.patch('setup.language', 'foobar')
+    def test_win(self):
+        lang = os.getenv('LANG')
+        reload(main)
+        self.assertEquals(os.getenv('LANG'), lang)
+        del os.environ['LANG']
+        reload(main)
+        self.assertEquals(os.getenv('LANG'), 'foobar')
+        os.environ['LANG'] = lang
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foo', 'bar'])
+    @mock.patch('main.log.error')
+    def test_too_many_args(self, mocklog):
+        main.run()
+        output = mocklog.call_args_list[0][0][0]
+        self.assertIn("Too many arguments", output)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foobar'])
+    @mock.patch('catatom2osm.CatAtom2Osm')
+    def test_default(self, mockcat):
+        main.run()
+        self.assertTrue(mockcat.called)
+        self.assertEquals(mockcat.call_args_list[0][0][0], 'foobar')
+        options = mockcat.call_args_list[0][0][1]
+        d = {'building': False, 'all': False, 'tasks': True, 'log_level': 'INFO', 
+            'parcel': False, 'list': False, 'zoning': True, 'version': False, 
+            'address': False}
+        for (k, v) in d.items():
+            self.assertEquals(getattr(options, k), v)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foobar', '-a'])
+    @mock.patch('catatom2osm.CatAtom2Osm')
+    def test_all(self, mockcat):
+        main.run()
+        self.assertTrue(mockcat.called)
+        options = mockcat.call_args_list[0][0][1]
+        d = {'building': True, 'all': True, 'tasks': True, 'log_level': 'INFO', 
+            'parcel': True, 'list': False, 'zoning': True, 'version': False, 
+            'address': True}
+        for (k, v) in d.items():
+            self.assertEquals(getattr(options, k), v)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foobar', '-b'])
+    @mock.patch('catatom2osm.CatAtom2Osm')
+    def test_building(self, mockcat):
+        main.run()
+        self.assertTrue(mockcat.called)
+        options = mockcat.call_args_list[0][0][1]
+        d = {'building': True, 'all': False, 'tasks': False, 'log_level': 'INFO', 
+            'parcel': False, 'list': False, 'zoning': False, 'version': False, 
+            'address': False}
+        for (k, v) in d.items():
+            self.assertEquals(getattr(options, k), v)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foobar', '--log=DEBUG'])
+    @mock.patch('main.log')
+    @mock.patch('catatom2osm.CatAtom2Osm')
+    def test_debug(self, mockcat, mocklog):
+        main.run()
+        self.assertTrue(mockcat.called)
+        options = mockcat.call_args_list[0][0][1]
+        self.assertEquals(options.log_level, 'DEBUG')
+        mocklog.setLevel.assert_called_once_with(logging.DEBUG)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foobar', '--log=foobar'])
+    @mock.patch('main.log.error')
+    @mock.patch('catatom2osm.CatAtom2Osm')
+    def test_bad_level(self, mockcat, mocklog):
+        main.run()
+        output = mocklog.call_args_list[0][0][0]
+        self.assertIn("Invalid log level", output)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', '-v'])
+    def test_version(self):
+        with capture(main.run) as output:
+            self.assertIn(setup.app_version, output)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', '-l', '33'])
+    @mock.patch('catatom2osm.list_municipalities')
+    def test_list(self, mocklist):
+        main.run()
+        mocklist.assert_called_once_with('33')
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', '-l', '33'])
+    @mock.patch('catatom2osm.list_municipalities', raiseIOError)
+    @mock.patch('main.log.error')
+    def test_list_error(self, mocklog):
+        main.run()
+        output = mocklog.call_args_list[0][0][0]
+        self.assertTrue(mocklog.called)
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foobar'])
+    @mock.patch('catatom2osm.CatAtom2Osm', raiseIOError)
+    @mock.patch('main.log.error')
+    def test_IOError(self, mocklog):
+        main.run()
+        output = mocklog.call_args_list[0][0][0]
+        self.assertEquals(output, 'bartaz')
+
+    @mock.patch('main.sys.argv', ['catatom2osm.py', 'foobar'])
+    @mock.patch('catatom2osm.CatAtom2Osm', raiseImportError)
+    @mock.patch('main.log.error')
+    def test_ImportError(self, mocklog):
+        main.run()
+        output1 = mocklog.call_args_list[0][0][0]
+        output2 = mocklog.call_args_list[1][0][0]
+        self.assertEquals(output1, 'qgis')
+        self.assertIn('install QGIS', output2)
