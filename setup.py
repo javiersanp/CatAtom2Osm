7ed5d8934d1b7ba48af2e1e5e9269ae94240beb5
Javier Sanchez <javiersanp@gmail.com>
2017-10-27 08:31:54 +0100

    Update docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import csv
 import gettext
 
 app_name = 'CatAtom2Osm'
-app_version = '2017-07-11'
+app_version = '2017-11-27shp'
 app_author = u'Javier Sánchez Portero'
 app_copyright = u'2017, Javier Sánchez Portero'
 app_desc = 'Tool to convert INSPIRE data sets from the Spanish Cadastre ATOM Services to OSM files'
