07ee3fbe86387cba07d2dcf287520c1d55f410a2
Javier Sánchez <javiersanp@gmail.com>
2017-12-04 22:45:16 +0000

    Fix typo

--- pyqgismac.sh
+++ pyqgismac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export QGISHOME=/Application/QGIS.app/Contents/MacOS
+export QGISHOME=/Applications/QGIS.app/Contents/MacOS
 export PATH="$QGISHOME/bin:$PATH"
 export PYTHONPATH="/Applications/QGIS.app/Contents/Resources/python:$PYTHONPATH"
 
